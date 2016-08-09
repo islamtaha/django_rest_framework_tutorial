7aff1bd3d11fe7c0006cb1e4d9357aff126ffde7
a.maher <ahmedmaherelmitwally@gmail.com>
2016-08-09 17:21:36 +0200

    part 3 added class based views

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,8 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 app_name = 'snippets'
 urlpatterns = [
-	url(r'^snippets/$', views.snippet_list, name='snippet_list'),
-	url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail, name='snippet_detail'),
+	url(r'^snippets/$', views.SnippetList.as_view(), name='snippet_list'),
+	url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(), name='snippet_detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
